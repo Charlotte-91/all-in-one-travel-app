934aefa802812b7b1be9c4fd448777c18d638bb5
Charlotte-91 <c.smallwood.smith@gmail.com>
2020-11-03 16:11:06 +0000

    Added view for login screen

--- AIO-travel/App.js
+++ AIO-travel/App.js
@@ -1,13 +1,19 @@
+//React Native elements //
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, Text, View } from 'react-native';
-import Startpage from './startPage';
+import React, { Component } from 'react';
+import { Button, Text, View, AppRegistry } from 'react-native';
+// Stylesheets//
 import styles from './StyleSheet';
+//Screens//
+import Startpage from './src/startPage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
+export default class App extends React.Component {
+
+  render(){
+    return (
       <Startpage/>
-    </View>
-  );
+    );
+  }
 }
+
+
--- AIO-travel/StyleSheet.js
+++ AIO-travel/StyleSheet.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#003f5c',
       alignItems: 'center',
       justifyContent: 'center',
     },
@@ -19,7 +19,42 @@ const styles = StyleSheet.create({
       borderBottomColor: '#737373',
       borderBottomWidth: StyleSheet.hairlineWidth,
     },
-  
+    logo: {
+      fontWeight:"bold",
+      fontSize:50,
+      color: "#0ADFC8",
+      marginBottom: 40
+    },
+    inputView:{
+      width: "80%",
+      backgroundColor:"#465881",
+      borderRadius:25,
+      height:50,
+      marginBottom:20,
+      justifyContent:"center",
+      padding:20
+    },
+    inputText:{
+      height:50,
+      color:"white"
+    },
+    forgot:{
+      color:"white",
+      fontSize:11
+    },
+    loginBtn:{
+      width: "80%",
+      backgroundColor: "#0ADFC8",
+      borderRadius:25,
+      height:50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop:40,
+      marginBottom:10
+    },
+    loginText:{
+      color:"white"
+    }
   });
   
 export default styles;
